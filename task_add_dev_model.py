a7aa1b3721061910ace54ec687c1cff59c4849aa
djask <ab0846591@tutanota.com>
2022-02-02 06:21:47 +0000

    change dev model script

--- task_add_dev_model.py
+++ task_add_dev_model.py
@@ -1,15 +1,29 @@
 # CPOC PBST template add model script
-from vmanage.api.device import Device
-from vmanage.api.policy_definitions import PolicyDefinitions
-from vmanage.api.device_templates import DeviceTemplates
+from vmanage.api.feature_templates import FeatureTemplates
 import pprint
+import sys
 
 from lib_vmanage import get_vmanage_auth
 
 auth, vmanage_host =  get_vmanage_auth('pbst2.ini')
-
-#vd = Device(auth, vmanage_host)
-vp = PolicyDefinitions(auth, vmanage_host)
-vdt = DeviceTemplates(auth, vmanage_host)
-
 pp = pprint.PrettyPrinter(indent=1, width=180)
+ft = FeatureTemplates(auth, vmanage_host)
+tlist = ft.get_feature_template_list()
+
+
+def add_device(new_dev):
+    for template in tlist:
+        print(template['deviceType'])
+        if new_dev not in template['deviceType']:
+            template['deviceType'].append(new_dev)
+            pp.pprint(ft.update_feature_template(template))
+
+def del_device(new_dev):
+    for template in tlist:
+        #print(template['deviceType'])
+        if new_dev in template['deviceType']:
+            template['deviceType'].remove(new_dev)
+            pp.pprint(ft.update_feature_template(template))
+
+new_dev = input("New device model to add to batch templates [vedge-ISR-4431]: ")
+add_device(new_dev)
